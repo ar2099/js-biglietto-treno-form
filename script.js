ed5e6d4940aa0bfbeb9e1375ac34d8688dbeb90e
Andrea <onemanshow2099@gmail.com>
2022-03-17 15:52:31 +0100

    quasi finito 2

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ let nome = document.getElementById("inputnomecognome");
 
 let vistaNome = document.getElementById("nome2");
 
+
+
 bottone.addEventListener("click",
 
     function () {
-        console.log()
+        
         vistaNome.innerHTML = nome.value ;
+
+        let carrozza = Math.floor(Math.random() * 6) + 1;
+        console.log(carrozza)
+        let carrozzaVista = document.getElementById("carrozza").innerHTML = carrozza;
+
+        let codice = Math.floor(Math.random() * 100) + 9000;
+        console.log(codice)
+        let codiceVista = document.getElementById("codice").innerHTML = codice;
+        
     }
 
 )
 
-// EVENTO
+
+// ANCHOR COSTO BIGLIETTO 
+
